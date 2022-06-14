d4f5fcdc40c6244c7d2792af15b4c2dcc40d9b3f
dyyyzzz <nuradilah688@gmail.com>
2022-06-14 23:43:32 +0800

    commit

--- add_user.php
+++ add_user.php
@@ -5,7 +5,7 @@ $sql="insert into user (first_name, last_name, email, pass_word)
 values ($_POST[firstname]','$_POST[lastname]','$_POST[email]', 
 '$_POST[password]', '$_POST[reg_date]')";
     if(!mysqli_query($conn, $sql)){
-        die("Error description: " . mysqli_error());
+        die("Connection failed: " . mysqli_connect_error());
     }
     echo "1 record successfully added...";
 
